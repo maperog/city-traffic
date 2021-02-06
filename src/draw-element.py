c1484cc8656acd3f52563bc15cef2eaba1780ec7
Cog <itz.cog@hotmail.com>
2021-02-06 22:09:55 +0800

    basic concepts for ui

---
+++ src/draw-element.py
@@ -0,0 +1,31 @@
+""" This program is under the terms of GNU Affero General Public License.
+    See LICENSE file for more detail.
+"""
+
+from tkinter import *
+
+# Constants
+VERTEX_RADIUS = 30
+EDGE_THICKNESS = 2
+VERTEX_COLOR = (150, 150, 150)
+EDGE_COLOR = (100, 100, 100)
+TEXT_COLOR = (0, 0, 0)
+
+# I have not decided on how to store a graph yet, this is just a placeholder
+graph = []
+
+# Basic Concepts. Cars are not considered yet
+def draw_vertex(x, y, index):
+    pass
+
+def connect_vertices(vertex1, vertex2, length):
+    pass
+
+def read_graph(filename):
+    pass
+
+def input_graph():
+    pass
+
+def draw_graph():
+    pass
