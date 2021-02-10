af7ff56d2d895ec56969f15a35295d1915c31363
Cog Ma <itz.cog@hotmail.com>
2021-02-10 21:05:02 +0800

    completed draw graph

--- src/draw-element.py
+++ src/draw-element.py
@@ -3,33 +3,51 @@
 """
 
 from tkinter import *
+import math
 
 # Constants
-SCREEN_SIZE = 600
-VERTEX_RADIUS = 30
-EDGE_THICKNESS = 2
+SCREEN_SIZE = 1000
 VERTEX_COLOR = "#969696"
-EDGE_COLOR = (100, 100, 100)
-TEXT_COLOR = (0, 0, 0)
-TEXT_FONT = "Arial 18"
+VERTEX_RADIUS = 15
+VERTEX_WIDTH = 3
+EDGE_COLOR = "#646464"
+EDGE_WIDTH = 4
+TEXT_FONT = "Arial 9"
 
-graph = []
+def application():
+    global graph
+    global tk, canvas
+    global rotation, dis, screen_center
+    
+    graph = []
 
-tk = Tk()
-tk.title("city-traffic")
-tk.geometry(str(SCREEN_SIZE) + "x" + str(SCREEN_SIZE))
-canvas = Canvas(tk, height=600, width=600)
-
+    '''rotation = 0
+    dis = 0
+    screen_center = 0'''
+    
+    tk = Tk()
+    tk.title("city-traffic")
+    tk.geometry(str(SCREEN_SIZE) + "x" + str(SCREEN_SIZE))
+    canvas = Canvas(tk, height=SCREEN_SIZE, width=SCREEN_SIZE)
+    
+    test_run()
+    print(dis)
+    while True:
+        canvas.pack()
+        tk.update()
 
 # Basic Concepts. Cars are not considered yet
 def draw_vertex(x, y, index):
-    vertex_index = canvas.create_oval(x-VERTEX_RADIUS, y-VERTEX_RADIUS,
-                                      x+VERTEX_RADIUS, y+VERTEX_RADIUS,
-                                      width=3, fill=VERTEX_COLOR)
+    canvas.create_oval(x-VERTEX_RADIUS, y-VERTEX_RADIUS,
+                       x+VERTEX_RADIUS, y+VERTEX_RADIUS,
+                       width=VERTEX_WIDTH, fill=VERTEX_COLOR)
     canvas.create_text(x, y, text=str(index), font=TEXT_FONT)
 
 def connect_vertices(vertex1, vertex2, length):
-    pass
+    line = canvas.create_line(*get_vertex_pos(vertex1),
+                              *get_vertex_pos(vertex2),
+                              width=EDGE_WIDTH)
+    canvas.tag_lower(line);
 
 def read_graph(filename):
     pass
@@ -37,10 +55,24 @@ def read_graph(filename):
 def input_graph():
     pass
 
-def draw_graph():
-    pass
+def draw_graph(vert_count, center_dis):
+    global rotation, dis, screen_center
+    rotation = 2 * math.pi / vert_count
+    dis = center_dis
+    screen_center = SCREEN_SIZE / 2
+    for i in range(0, vert_count):
+        draw_vertex(*get_vertex_pos(i), i)
 
-draw_vertex(300, 300, 16)
-while True:
-    canvas.pack()
-    tk.update()
+def get_vertex_pos(vert_index):
+    return (screen_center+dis*math.cos(rotation*vert_index),
+            screen_center+dis*math.sin(rotation*vert_index))
+
+def test_run():
+    draw_graph(45, 450)
+    connect_vertices(1, 36, 1)
+    connect_vertices(4, 12, 1)
+    connect_vertices(13, 23, 1)
+    connect_vertices(1, 2, 1)
+    connect_vertices(19, 41, 1)
+
+application();
