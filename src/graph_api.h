7a82ec1b2b94d2296cac6270bb8f983a8d4d2539
ferrumcccp <wushuzhwn1975@icloud.com>
2021-02-26 21:52:49 +0800

    Work on graph_api.h

--- src/graph_api.h
+++ src/graph_api.h
@@ -16,10 +16,28 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>
  */
 
-#include "road.h" // Highly WIP, caution!
+#ifndef GRAPH_API_H_INCLUDED
+#define GRAPH_API_H_INCLUDED
+struct VertexMeta{ // Add vertex metadata here
+    int junction_type;
+};
+struct EdgeMeta{ // Add edge metadata here
+    int len; //length
+    int quality; // a factor determining vehicle speed
+    int ord1; // The order of the edge on the vertex s
+    // Since the junction requires the pins and ports to
+    // be given in the correct order, the backend should
+    // sort the edges according to ord1 and ord2.
+    // For the same order value, the edge first added goes
+    // first (Use std::stable_sort)
+
+    int ord2; // The order of the edge on the vertex t
+};
+bool edge_meta_cmp1(EdgeMeta x,EdgeMeta y){return x.ord1<y.ord1;}
+bool edge_meta_cmp2(EdgeMeta x,EdgeMeta y){return x.ord2<y.ord2;}
 
 void set_nm(int n,int m); // n vertices and m edges
-void set_tl_type(int v,int x); // for vertex v set traffic light type to x
-void add_tl_offset(int v,int t); // add offset to the traffic light
-int add_edg(int s,int t,Road meta); //add undirected edge between s and t with road data meta, returns edge id
-int fix_id(int id); //get the canonical edge id
+void setup_vertex(VertexMeta mt); // Set up a vertex with vertex metadata my
+int add_edg(int s,int t,EdgeMeta mt); //add undirected edge between s and t with edge metadata mt, returns edge id
+//int fix_id(int id); //get the canonical edge id
+#endif
