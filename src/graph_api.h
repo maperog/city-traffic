03baf770ab095b71f0f45d71fc30597b49e02e8d
ferrumcccp <wushuzhwn1975@icloud.com>
2021-03-07 18:52:40 +0800

    Add query to graph api

--- src/graph_api.h
+++ src/graph_api.h
@@ -15,13 +15,17 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>
  */
+#include<vector>
+#include<utility>
 
 #ifndef GRAPH_API_H_INCLUDED
 #define GRAPH_API_H_INCLUDED
-struct VertexMeta{ // Add vertex metadata here
+struct Vertex{ // Add vertex metadata here
     int junction_type;
 };
-struct EdgeMeta{ // Add edge metadata here
+struct EdgeMeta{ // Add edge data here
+    int s; // Starting Point
+    int t; // Finishing point
     int len; //length
     int quality; // a factor determining vehicle speed
     int ord1; // The order of the edge on the vertex s
@@ -33,11 +37,29 @@ struct EdgeMeta{ // Add edge metadata here
 
     int ord2; // The order of the edge on the vertex t
 };
-bool edge_meta_cmp1(EdgeMeta x,EdgeMeta y){return x.ord1<y.ord1;}
-bool edge_meta_cmp2(EdgeMeta x,EdgeMeta y){return x.ord2<y.ord2;}
+bool edge_cmp1(Edge x,Edge y){return x.ord1<y.ord1;}
+bool edge_cmp2(Edge x,Edge y){return x.ord2<y.ord2;}
 
 void set_nm(int n,int m); // n vertices and m edges
-void setup_vertex(VertexMeta mt); // Set up a vertex with vertex metadata my
-int add_edg(int s,int t,EdgeMeta mt); //add undirected edge between s and t with edge metadata mt, returns edge id
+void setup_vertex(Vertex mt); // Set up a vertex with vertex metadata my
+int add_edg(Edge mt); //add undirected edge between s and t with edge metadata mt, returns edge id
 //int fix_id(int id); //get the canonical edge id
+Edge get_edg_by_id(int id); // Get edge data by ID
+const std::vector<int>&list_edg(int s); // List edges by starting point
+const int*c_list_edg(int s){ // List edges by starting point (C-style array) Terminating with -1
+    std::vector<int> v=list_edg(s);
+    int *arr=new int[v.size()+1];
+    std::copy(v.begin(),v.end(),arr);
+    arr[v.size()]=-1;
+    return arr;
+}
+
+// For GUI
+int get_canvas_x();
+int get_canvas_y(); // Canvas size
+std::pair<int,int>recommend_coord(int p); // return recommended coordinate of point p
+long long int c_recommend_coord(int p){
+    std::pair<int,int>r=recommend_coord(p);
+    return (r.first<<32)&r.second;
+}
 #endif
